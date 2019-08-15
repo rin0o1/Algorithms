c495371542164484e6b3706ecf1d5b0eaea6b415
rin0o1 <30932379+rin0o1@users.noreply.github.com>
2019-08-15 08:10:27 +0100

    Binary Search

--- Main.py
+++ Main.py
@@ -1,72 +1,38 @@
+#Binary search
 
-#Find those 2 number in an unsorted array that their sum is like x
-#Step 1- sort array
-#Step 2- find numbers
-
-
-class Utilities:
-    def __init__(self, arr):
-
-        self.Sort(arr, 0, len(Arr)-1)
+from QuickSort import QuickSort
 
 
-    def Sort(self, arr, min,max):
+def SearchElementInArray(arr, element, min,max):
+    #Base case of recursion
+    if max >= min:
+        #Find the index of half array
+        MidElement= min+(max-min)//2
 
-        if min<max:
-
-            Pivot= self.Partition(arr, min, max)
-
-            #Sort Right
-            self.Sort(arr,Pivot+1, max)
-            #Sort.Left
-            self.Sort(arr, min, Pivot-1)
-
-
-    def Partition(self, arr, min, max):
-
-        IndexMinValue = min-1
-        Pivot = arr[max]
-
-        for Element in range( min, max):
-
-            if arr[Element] < Pivot:
-                IndexMinValue = IndexMinValue+1
-                arr[Element], arr[IndexMinValue] = arr[IndexMinValue], arr[Element]
-
-        PivotPosition= IndexMinValue+1
-        arr[PivotPosition] , arr[max] = arr[max], arr[PivotPosition]
-        return PivotPosition
-
-
-#Check if extists 2 numbers that they sum is x
-def FindSum(x, arr, LeftIndex, RightIndex):
-    ResultArr=[]
-    while LeftIndex < RightIndex:
-        ElementLeft=arr[LeftIndex]
-        ElementRight= arr[RightIndex]
-
-        Sum= ElementLeft+ElementRight
-
-        if Sum == x:
-            ResultArr.append(ElementRight)
-            ResultArr.append(ElementLeft)
-            LeftIndex= LeftIndex+1
-            RightIndex= RightIndex+1
-        elif Sum <x:
-            LeftIndex= LeftIndex+1
-        else :
-            RightIndex= RightIndex-1
-    return  ResultArr
+        #If the element to find catch with "MidElement" return it
+        if arr[MidElement]==element:
+            return  MidElement
+        #if the element to find is less than element in "MidElement" index search just on the left way
+        elif arr[MidElement]>element:
+            return  SearchElementInArray(arr, element,min, MidElement-1)
+        #else search just on the right way
+        else:
+            return SearchElementInArray(arr, element, MidElement+1, max)
+    else:
+        return  -1
 
 
 
 if __name__=='__main__':
-    Arr=[45,10,6,5,1,-5,-10]
-    Utilities_ = Utilities(Arr)
-    LeftIndex=0
-    RightIndex= len(Arr)-1
-    x=11
-    print(FindSum(x, Arr, LeftIndex, RightIndex))
+    Arr=[14,17,16,18, 6]
+    x=17
+    Sort= QuickSort(Arr)
+    res= SearchElementInArray(Arr, x, 0, len(Arr)-1)
+    print(res)
+
+
+
+
 
 
 
