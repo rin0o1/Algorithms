ba4cac8b69b9bbda48bd2a129117a64626f6b8a6
rin0o1 <30932379+rin0o1@users.noreply.github.com>
2019-11-08 17:25:02 +0100

    MergeSort

--- Main.py
+++ Main.py
@@ -1,44 +1,47 @@
-#Binary search
-
-from QuickSort import QuickSort
 
 
-def SearchElementInArray(arr, element, min,max):
-    #Base case of recursion
-    if max >= min:
-        #Find the index of half array
-        MidElement= min+(max-min)//2
+def MergeSort(Arr):
 
-        #If the element to find catch with "MidElement" return it
-        if arr[MidElement]==element:
-            return  MidElement
-        #if the element to find is less than element in "MidElement" index search just on the left way
-        elif arr[MidElement]>element:
-            return  SearchElementInArray(arr, element,min, MidElement-1)
-        #else search just on the right way
-        else:
-            return SearchElementInArray(arr, element, MidElement+1, max)
-    else:
-        return  -1
+    if (len(Arr)>1):
+        Mid= len(Arr) // 2
+        LeftSize= Arr[:Mid]
+        RightSize= Arr[Mid:]
 
+        MergeSort(LeftSize)
+        MergeSort(RightSize)
+
+        IndexLeft=0
+        IndexRight=0
+        IndexArr=0
+
+        while IndexLeft< len(LeftSize) and IndexRight <len(RightSize):
+            if LeftSize[IndexLeft]< RightSize[IndexRight]:
+                Arr[IndexArr]= LeftSize[IndexLeft]
+                IndexLeft+=1
+            else:
+                Arr[IndexArr]= RightSize[IndexRight]
+                IndexRight+=1
+            IndexArr+=1
+
+        while IndexLeft< len(LeftSize):
+            Arr[IndexArr]= LeftSize[IndexLeft]
+            IndexArr+=1
+            IndexLeft+=1
+        while IndexRight< len(RightSize):
+            Arr[IndexArr]= RightSize[IndexRight]
+            IndexArr+=1
+            IndexRight+=1
+
+
+    pass
 
 
 if __name__=='__main__':
-    Arr=[14,17,16,18, 6]
-    x=17
-    Sort= QuickSort(Arr)
-    res= SearchElementInArray(Arr, x, 0, len(Arr)-1)
-    print(res)
-
-
-
-
-
-
-
-
-
-
-
-
-
+    TestCases= int(input())
+    for _  in range(TestCases):
+        Lenght = int(input())
+        arr = list(map(int, input().split()))
+        MergeSort(arr)
+        for i in range(len(arr)):
+            print(arr[i], end=" ")
+        print()
